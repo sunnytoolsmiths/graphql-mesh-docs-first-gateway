ab2003c2bfa21cafa57c2d72cb4105ea12e78f3b
Sunny Xia <sunnyxia@Sunnys-MacBook-Air.local>
2024-05-02 17:44:34 -0700

    fix the configuration

---
+++ my-package/README.md
@@ -0,0 +1,17 @@
+Will Farley
+===========
+Javascript and Python Web Engineer
+
+```
+I write code.
+```
+
+### Connect with me
+* [github.com/goldhand][github]
+* [a.will.farley@gmail.com][email]
+* [www.willfarley.org][website]
+
+
+[github]: https://github.com/goldhand/goldhand
+[website]: http://www.willfarley.org
+[email]: mailto:a.will.farley@gmail.com
---
+++ my-package/index.js
@@ -0,0 +1,15 @@
+function selfPromote() {
+  console.log('\n\n');
+  console.log('**********************************************');
+  console.log('**********************************************');
+  console.log('***************  Will Farley  ****************');
+  console.log('***********  github.com/goldhand/  ***********');
+  console.log('************  www.willfarley.org  ************');
+  console.log('**********************************************');
+  console.log('**********************************************');
+  console.log('\n\n');
+}
+
+selfPromote();
+
+module.exports = selfPromote;
